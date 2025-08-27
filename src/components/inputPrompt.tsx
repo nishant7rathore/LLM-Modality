3a82cd94577f6f5b337d692247e0f725118d2201
Nishant Rathore <nishant7rathore@gmail.com>
2025-08-27 16:58:53 -0230

    removed hours from formattedTime button

--- src/components/inputPrompt.tsx
+++ src/components/inputPrompt.tsx
@@ -87,15 +87,13 @@ const InputPrompt = ({
   }
 
   const formatTime = (seconds: number) => {
-    const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     const remainingSeconds = seconds % 60;
 
-    const formattedHours = String(hours).padStart(2, '0');
     const formattedMinutes = String(minutes).padStart(2, '0');
     const formattedSeconds = String(remainingSeconds).padStart(2, '0');
 
-    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+    return `${formattedMinutes}:${formattedSeconds}`;
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
